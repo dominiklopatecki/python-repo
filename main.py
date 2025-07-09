bf26c3328e2d7ee9e785a5b1d8182429591ebfdc
dominiklopatecki <dominiklopatecki36@gmail.com>
2025-07-09 17:18:10 +0200

    inital commit

---
+++ main.py
@@ -0,0 +1,13 @@
+
+
+title = "Mój nowy plik"
+
+# Tworzenie lokalnego repozytorium
+# git init
+
+#sprawdzenie statusu plików
+# git status
+
+#dod pliku do repo lokal
+# git add nazwa_pliku
+#git add . (dodanie wszystkiego)
